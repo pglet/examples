60b9b4def617dd1ce3b5310b5c7c74c57df192c7
Feodor Fitsner <feodor@appveyor.com>
2021-01-08 14:07:49 -0800

    hello.js

---
+++ node/hello-world/hello.js
@@ -0,0 +1,10 @@
+const pglet = require("pglet");
+
+(async () => {
+    p = await pglet.page("hello");
+
+    await p.send("clean");
+    await p.send("add text value='Hello, world!'");
+})();
+
+console.log('Done!')
