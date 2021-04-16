eaa47e99870f9d7b193d7c1f7499f58b287b90ed
Feodor Fitsner <feodor@appveyor.com>
2021-04-16 11:57:36 -0700

    Python ToDo app added

--- python/hello-world/hello-app.py
+++ python/hello-world/hello-app.py
@@ -1,12 +1,7 @@
 import pglet
 from pglet import Text
 
-class HelloWorldApp:
-    def __init__(self, p):
-        self.p = p
-        self.main()
-    
-    def main(self):
-        self.p.add(Text(value=f"Hello to session {self.p.conn_id}!"))
+def main(page):
+    page.add(Text(f"Hello to session {page.connection.conn_id}!"))
 
-pglet.app(target=HelloWorldApp, web=True)
+pglet.app(target=main, web=True)
