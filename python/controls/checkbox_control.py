8d53a813796e48bdad8addf3c4b3604828ade9aa
Inesa Fitsner <inesa@appveyor.com>
2021-04-21 12:21:59 -0700

    Create checkbox_control.py

---
+++ python/controls/checkbox_control.py
@@ -0,0 +1,34 @@
+import pglet
+from pglet import Stack, Text, Checkbox
+
+def checkboxes():
+  return Stack(gap=20, controls=[
+    Text("Checkboxes", size="xLarge"),
+    Checkbox(label='Unchecked checkbox', value=False),
+    Checkbox(label='Checked checkbox', value=True),
+    Checkbox(label='Disabled checkbox', disabled=True),
+    Checkbox(label="Checkbox with rendered box_side='End'", box_side='End'),
+    checkbox_with_on_change()
+  ])
+
+def checkbox_with_on_change():
+
+  def checkbox_changed(e):
+    t.value = f"Checkbox value changed to {c.value}" 
+    stack.update()
+
+  c = Checkbox('Checkbox with on_change event', on_change=checkbox_changed)
+  t = Text()
+  stack = Stack(controls=[c, t])
+  return stack
+
+
+def main(page):
+
+    page.title = "Checkbox control samples"
+    page.update()
+    page.add(checkboxes())
+
+pglet.app("checkbox-control-samples", target = main)
+
+input("Press Enter to exit...")
