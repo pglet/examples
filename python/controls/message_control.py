b76af7c8b4604ecbf4a1c0eded71199635a6e3db
Inesa Fitsner <inesa@appveyor.com>
2021-04-21 19:09:25 -0700

    slider control

--- python/controls/message_control.py
+++ python/controls/message_control.py
@@ -55,20 +55,3 @@ def main(page):
 pglet.app("message-control-samples", target = main)
 
 input("Press Enter to exit...")
-
-'''
-function main() {
-  pglet_send "set page horizontalAlign='stretch'"
-  pglet_send "add
-  message value='This is just a message.'
-  message success value='All files have been successfully copied.' dismiss
-  message error value='User with specified email was not found.' dismiss
-  message blocked truncated value='Blocked MessageBar - single line, with dismiss button and truncated text. Truncation is not available if you use action buttons or multiline and should be used sparingly. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi luctus, purus a lobortis tristique, odio augue pharetra metus, ac placerat nunc mi nec dui. Vestibulum aliquam et nunc semper scelerisque. Curabitur vitae orci nec quam condimentum porttitor et sed lacus. Vivamus ac efficitur leo. Cras faucibus mauris libero, ac placerat erat euismod et. Donec pulvinar commodo odio sit amet faucibus. In hac habitasse platea dictumst. Duis eu ante commodo, condimentum nibh pellentesque, laoreet enim. Fusce massa lorem, ultrices eu mi a, fermentum suscipit magna. Integer porta purus pulvinar, hendrerit felis eget, condimentum mauris. You\'ve been warned!' dismiss
-  message warning dismiss value='Watch out! You\'ve been warned!'
-    button text=Yes action=yes
-    button text=No action=no
-  message warning multiline value='Warning defaults to multiline. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi luctus, purus a lobortis tristique, odio augue pharetra metus, ac placerat nunc mi nec dui. Vestibulum aliquam et nunc semper scelerisque. Curabitur vitae orci nec quam condimentum porttitor et sed lacus. Vivamus ac efficitur leo. Cras faucibus mauris libero, ac placerat erat euismod et. Donec pulvinar commodo odio sit amet faucibus. In hac habitasse platea dictumst. Duis eu ante commodo, condimentum nibh pellentesque, laoreet enim. Fusce massa lorem, ultrices eu mi a, fermentum suscipit magna. Integer porta purus pulvinar, hendrerit felis eget, condimentum mauris.'
-    button text=OK
-    button text=Cancel  
-  "
-'''
