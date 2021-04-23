8fb71c0d4eeac9b7922d4107f676c9f150e590ac
Inesa Fitsner <inesa@appveyor.com>
2021-04-23 11:18:27 -0700

    nav added

--- python/controls/toggle_control.py
+++ python/controls/toggle_control.py
@@ -1,20 +1,6 @@
 import pglet
 from pglet import Stack, Text, Toggle
 
-'''
-
-  pglet_send "add
-    toggle label='Enabled and checked' value='true'
-    toggle label='Enabled and unchecked'
-    toggle disabled label='Disabled and checked' value='true'
-    toggle disabled label='Disabled and unchecked'
-    toggle inline label='With inline label' onText=On offText=Off
-    toggle disabled inline label='Disabled with inline label' onText=On offText=Off
-    toggle inline label='With inline label and without onText and offText'
-    toggle disabled inline label='Disabled with inline label and without onText and offText'  
-  "
-'''
-
 def toggles(page):
   return Stack(controls=[
         Text("Toggles", size="xLarge"),
