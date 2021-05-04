da0404b4ce44860e2758f328b2652de217e4c2e3
Owen McDonnell <omcdonnell@gmail.com>
2021-05-04 11:45:26 -0700

    TodoApp initial commit, using local (relative path)  pglet build

---
+++ node/todo/app.js
@@ -0,0 +1,49 @@
+const pglet = require('../../../pglet-node/build/index.js');
+
+const Text = pglet.Text;
+const Stack = pglet.Stack;
+const Textbox = pglet.Textbox;
+const Button = pglet.Button;
+const Checkbox = pglet.Checkbox;
+const Tabs = pglet.Tabs;
+const Tab = pglet.Tab;
+
+
+class Task {
+    constructor(app, name) {
+        this.app = app;
+        this.displayTask = new Checkbox({value: false, label: name, onChange: this.statusChanged});
+        this.editname = Textbox()
+    }
+}
+class TodoApp {
+    constructor(page) {
+        this.page = page;
+        this.newTask = new Textbox({placeholder: "What needs doing?"});
+        this.tasksView = new Stack({});
+        this.view = new Stack({ width: '70%', childControls: [
+            new Stack({ horizontal: true, childControls: [
+                this.newTask,
+                new Button({text: 'Add', onClick: this.addClicked.bind(this)})
+            ] }),
+            this.tasksView
+        ] });
+    }
+    async addClicked(e) {
+        let taskValue = await this.page.getValue(this.newTask); //this.newTask.value 
+        let checkBox = new Checkbox({value: false, label: taskValue});
+        this.tasksView.childControls.push(checkBox);
+        this.page.update();
+    }
+}
+
+async function main(page) {
+    page.title = "ToDo app";
+    page.horizontalAlign = 'center';
+    await page.update();
+    let app = new TodoApp(page);
+    page.add([app.view]);
+
+}
+
+pglet.app("todo-app", main);
--- python/greeter/greeter.py
+++ python/greeter/greeter.py
@@ -11,7 +11,8 @@ def say_hello_click(e):
     p.add(Text(value=f'Hello, {name}!'))
     sys.exit()
 
-txt_name = p.add(Textbox(label="Your name", description="Please provide your full name"))
+txt_name = p.add(Textbox(label="Your name", description="Please provide your full name"), Textbox(label="Your name", description="Please provide your full name"))
+print("txt_name ", txt_name)
 p.add(Button(text="Say hello", primary=True, onclick=say_hello_click))
 
 # wait until browser window is closed or page reloaded
