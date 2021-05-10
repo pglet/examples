f7339afbfea212320e2ba2461b1f07c68cad5ff5
Inesa Fitsner <inesa@appveyor.com>
2021-05-10 09:26:59 -0700

    Update change_items_in_choicegroup.py

--- python/controls/controls _for_gallery/ChoiceGroup/change_items_in_choicegroup.py
+++ python/controls/controls _for_gallery/ChoiceGroup/change_items_in_choicegroup.py
@@ -9,26 +9,22 @@ def change_items_in_choicegroup():
   def add_clicked(e):
     cg.options.append(choicegroup.Option(option_name.value))
     option_name.value = ''
-    option_name.error_message = ''
     stack.update()
 
   def delete_clicked(e):
-    option_found = False
     for option in cg.options:
-        if option.key == option_name.value:
-          cg.options.remove(option)
-          option_found = True
-          option_name.error_message = ''
-    if option_found == False:
-        option_name.error_message = 'Option not found'
-    option_name.value = ''
+        if option.key == cg.value:
+            cg.options.remove(option) 
     stack.update() 
 
   cg = ChoiceGroup()
   option_name = Textbox(placeholder='Enter option name')
   add = Button("Add", on_click=add_clicked)
-  delete = Button("Delete", on_click=delete_clicked)
-  stack = Stack(controls = [cg, Stack(horizontal=True, controls=[option_name, add, delete])])
+  delete = Button("Delete selected", on_click=delete_clicked)
+  stack = Stack(horizontal_align='start', controls = [
+      Stack(horizontal=True, controls=[option_name, add]),
+      cg,
+      delete])
   return stack
  
 stack = Stack(gap=20, controls=[
