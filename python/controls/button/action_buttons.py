733635c46c8e71e205b6ecd066047776ea03bc5e
Inesa Fitsner <inesa@appveyor.com>
2022-02-03 12:40:25 -0800

    action button file renamed

--- python/controls/button/action_buttons.py
+++
@@ -1,9 +0,0 @@
-import pglet
-from pglet import Button, Stack
-with pglet.page("action-buttons") as page:
-  page.add(Stack(horizontal=True, controls=[
-    Button(action=True, icon='ChevronLeftEnd6'),
-    Button(action=True, icon='ChevronLeftSmall'),
-    Button(action=True, icon='ChevronRightSmall'),
-    Button(action=True, icon='ChevronRightEnd6')
-  ]))
