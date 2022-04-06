8af340e6620482748f370d83d03eb9ba26490ee2
Inesa Fitsner <inesa@appveyor.com>
2022-04-06 09:31:22 -0700

    basic dialog updated

--- python/controls/dialog/basic_dialog.py
+++ python/controls/dialog/basic_dialog.py
@@ -1,22 +1,23 @@
 import pglet
-from pglet import Dialog, Button, Text, Stack
+from pglet import Button, Checkbox, Dialog, Text
+
 with pglet.page("basic-dialog") as page:
-      
-  def button_clicked(e):
+
+    def button_clicked(e):
         d.open = True
         page.update()
 
+    b = Button(text="Open dialog", on_click=button_clicked)
+    page.add(b)
 
-  b = Button(text='Open dialog', on_click=button_clicked)
-  page.add(b)
+    d = Dialog(
+        title="Welcome!",
+        width=200,
+        height=100,
+        controls=[Text(size="small", align="center", value="This is a basic dialog")],
+        footer=[Button("OK"), Button("Cancel")],
+    )
 
-  d = Dialog(title="Welcome!", width=200, height=100, controls=[
-        Text(size='small', align='center', value='This is a basic dialog')],
-        footer=[Button("OK"),Button("Cancel")]
-  )
-  
-  page.add(d)
+    page.add(d)
 
-  input()
-  
-  
+    input()
